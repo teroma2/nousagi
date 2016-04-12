209e1742dd6e7387f03dc5992aa0b89424a79bfc
ヨシマツカヨコ <teroma2@teromatsu-no-MacBook-Air.local>
2016-04-13 03:10:42 +0900

    メンテしやすいように記事ページの条件分岐とページャーの書き方を変更した

--- www/wordpress/wp-content/themes/twentysixteen-child/single.php
+++ www/wordpress/wp-content/themes/twentysixteen-child/single.php
@@ -11,39 +11,26 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-		<?php
-		// Start the loop.
-		while ( have_posts() ) : the_post();
-
-			// Include the single post content template.
-			get_template_part( 'template-parts/content', 'single' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) {
-				comments_template();
-			}
-
-			if ( is_singular( 'attachment' ) ) {
-				// Parent post navigation.
-				the_post_navigation( array(
-					'prev_text' => _x( '<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', 'twentysixteen' ),
-				) );
-			} elseif ( is_singular( 'post' ) ) {
-				// Previous/next post navigation.
-				the_post_navigation( array(
-					'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentysixteen' ) . '</span> ' .
-						'<span class="screen-reader-text">' . __( 'Next post:', 'twentysixteen' ) . '</span> ' .
-						'<span class="post-title">%title</span>',
-					'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentysixteen' ) . '</span> ' .
-						'<span class="screen-reader-text">' . __( 'Previous post:', 'twentysixteen' ) . '</span> ' .
-						'<span class="post-title">%title</span>',
-				) );
-			}
-
-			// End of the loop.
-		endwhile;
-		?>
-
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+    <?php if ( comments_open() || get_comments_number() ): ?>
+      <?php comments_template(); ?>
+    <?php endif; ?>
+    <?php if ( is_singular( 'attachment' ) ): ?>
+        the_post_navigation( array(
+          'prev_text' => _x( '<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', 'twentysixteen' ),
+    <?php elseif ( is_singular( 'post' ) ): ?>
+			▼記事のページャー
+			<ul class="post-pager">
+				<li class="post-title">
+					<?php previous_post_link('&laquo; %link', '前の記事へ', true); ?>
+				</li>
+				<li class="post-title">
+					<?php next_post_link('%link &raquo;', '次の記事へ', true); ?>
+				</li>
+			</ul>
+    <?php endif; ?>
+    <?php endwhile; ?>
 	</main><!-- .site-main -->
 
 	<?php get_sidebar( 'content-bottom' ); ?>
